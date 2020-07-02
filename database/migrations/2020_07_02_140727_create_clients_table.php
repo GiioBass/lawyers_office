c67da3c92f165cad3ccbd1cae12bc5801dc5ce71
Geovanny <williamnavarrocanon@gmail.com>
2020-07-02 09:45:04 -0500

    create migrations, controllers, seeders, models

---
+++ database/migrations/2020_07_02_140727_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->unsignedInteger('id')->unique();
+            $table->unsignedInteger('document_number')->unique();
+            $table->string('names', 100);
+            $table->string('lastname', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
