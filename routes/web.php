e71b0b33cfe9d73214fdf6acf4851438d8b53b20
Geovanny <williamnavarrocanon@gmail.com>
2020-07-02 23:22:48 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
-    return view('Affairs/index');
-})->name('home');
+    return redirect()->route('affair.index');
+});
 
 Route::resource('/client', 'ClientController');
 Route::resource('/lawyer', 'LawyerController');
